30fa9a0940480a94af89908422b3b080013865a2
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-27 19:42:30 +0100

    GOlang

--- Chapter Four/taxCalculator.go
+++ Chapter Four/taxCalculator.go
@@ -1,10 +1,10 @@
-package main
+ppackage main
 
-import (
-	"fmt"
-)
+import "fmt"
+
 
 func main() {
+	fmt.Print("Our system can determine deu tax of Three person's per time!!")
 	for i := 0; i < 3; i++ {
 		var name string
 		var earning float64
@@ -15,22 +15,21 @@ func main() {
 		fmt.Printf("Enter %s's earning: ", name)
 		fmt.Scanln(&earning)
 
-		tax := calculateTax(earning)
+		benchPay1 := 30000.0
+		taxRate1 := 0.15
+		taxRate2 := 0.20
+
+		var tax float64
+
+		if earning > benchPay1 {
+			benchPayTax := earning * taxRate1
+			benchPay2 := earning - benchPay1
+			smallTax := benchPay2 * taxRate2
+			tax = benchPayTax + smallTax
+		} else {
+			tax = earning * taxRate1
+		}
+
 		fmt.Printf("%s's Total Tax: %.2f\n", name, tax)
 	}
-}
-
-func calculateTax(earning float64) float64 {
-	benchPay1 := 30000.0
-	taxRate1 := 0.15
-	taxRate2 := 0.20
-
-	if earning > benchPay1 {
-		benchPayTax := earning * taxRate1
-		benchPay2 := earning - benchPay1
-		smallTax := benchPay2 * taxRate2
-		return benchPayTax + smallTax
-	}
-
-	return earning * taxRate1
 }
