db08b41b5cfff7574173dde98eaefae23164165b
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-30 00:58:15 +0100

    GO

--- Chapter Five/trianglePint.go
+++ Chapter Five/trianglePint.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// Print ascending triangle
+	
 	for count := 1; count <= 10; count++ {
 		for j := 1; j <= count; j++ {
 			fmt.Print("*")
@@ -11,7 +11,7 @@ func main() {
 		fmt.Println()
 	}
 
-	// Print descending triangle
+
 	for count := 10; count >= 1; count-- {
 		for j := 1; j <= count; j++ {
 			fmt.Print("*")
